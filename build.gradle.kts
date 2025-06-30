44795a4d90dd4e965dae730042340bf00325bad8
Naman Malhotra <mnaman.gonnect@gmail.com>
2025-06-30 16:22:55 +0100

    Completed checkpoint merge of code

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,6 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-    implementation(files("C:/Users/andre/Downloads/lcrypto-jdk18on-1.81/lcrypto-jdk18on-1.81/src"))
 }
 
 tasks.test {
