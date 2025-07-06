8b4c0acd60b3020d3769bce6e5be715b4b3153ce
Naman Malhotra <mnaman.gonnect@gmail.com>
2025-07-06 18:57:14 +0100

    Debugging eccMul and improved stategy

--- src/main/java/crypto/Test.java
+++ src/main/java/crypto/Test.java
@@ -3,6 +3,8 @@ package crypto;
 import constants.Params;
 import crypto.core.ECC;
 import exceptions.EncryptionException;
+import field.operations.FP;
+import field.operations.FP2;
 import types.data.F2Element;
 import types.point.AffinePoint;
 import types.point.ExtendedPoint;
@@ -29,11 +31,16 @@ public class Test {
         ExtendedPoint genExt = generator.toExtendedPoint();
 //        System.out.println(ECC.eccPointValidate(genExt));
 //        System.out.println(Params.PARAMETER_d.real + " i " + Params.PARAMETER_d.im);
-        System.out.println(convertToField(generator).getX());
-        System.out.println(convertToField(generator).getY());
-        System.out.println(ECC.eccMul(convertToField(generator), BigInteger.ONE, false).getX());
-        System.out.println(ECC.eccMul(convertToField(generator), BigInteger.ONE, false).getY());
+//        System.out.println(convertToField(generator).getX());
+//        System.out.println(convertToField(generator).getY());
+        System.out.println(ECC.eccMul(convertToField(generator), BigInteger.ONE, false));
+//        System.out.println(ECC.eccMul(convertToField(generator), BigInteger.ONE, false).getY());
 //        System.out.println(Params.T_VARBASE);
+//        System.out.println(FP2.fp2Add1271(
+//                new F2Element(BigInteger.valueOf(2910894007959172010), new 10846532345338296310i,),
+//                new F2Element(y=13340966418444432007 + 5307425384924220252i),
+//        ));
+        System.out.println(new BigInteger("1a3472237c2fb305286592ad7b3833aa", 16));
     }
 
     private static FieldPoint convertToField(AffinePoint affine) {
