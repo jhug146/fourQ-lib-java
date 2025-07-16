944bf6222efa75ccb3a0e05421ce1e10569cd599
Naman Malhotra <mnaman.gonnect@gmail.com>
2025-07-16 22:58:29 +0100

    Implemented provisional KangarooTwelve Hashing implementation and added Jar in order to facilitate import

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,9 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     implementation("org.jetbrains:annotations:24.1.0")
+
+    // BouncyCastle dependencies for the K12 HashFunction
+    implementation(files("lib/bcprov-jdk18on-1.81.jar"))
 }
 
 tasks.test {
