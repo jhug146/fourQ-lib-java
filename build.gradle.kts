cfbcd295e7d27ce87e2608aaf2f78950f73e9c04
Naman Malhotra <mnaman.gonnect@gmail.com>
2025-07-23 11:23:24 +0530

    Deployed the library to Maven Central and related pom configurations.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("com.vanniktech.maven.publish") version "0.34.0"
 }
 
 group = "org.example"
@@ -7,6 +8,46 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+}
+
+// This publishing can only occur if the local gradle.properties includes the correct secrets.
+mavenPublishing {
+    publishToMavenCentral()
+    signAllPublications()
+    coordinates("io.github.malhotranaman", "fourQ-lib-java", "1.0.0")
+
+    pom {
+        name.set("FourQ Library Java")
+        description.set("FourQlib is an efficient and portable cryptographic library that provides functions for computing elliptic curve based operations on the high-performance FourQ curve. This is a Java implementation that aims to minimize native performance lost.")
+        inceptionYear.set("2025")
+        url.set("https://github.com/malhotranaman/fourQ-lib-java")
+
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+
+        developers {
+            developer {
+                id.set("malhotranaman")
+                name.set("Naman Malhotra")
+                url.set("https://github.com/malhotranaman")
+            }
+            developer {
+                id.set("jhug146")
+                name.set("James Hughff")
+                url.set("https://github.com/jhug146")
+            }
+        }
+
+        scm {
+            url.set("https://github.com/malhotranaman/fourQ-lib-java")
+            connection.set("scm:git:git://github.com/malhotranaman/fourQ-lib-java.git")
+            developerConnection.set("scm:git:ssh://git@github.com/malhotranaman/fourQ-lib-java.git")
+        }
+    }
 }
 
 dependencies {
